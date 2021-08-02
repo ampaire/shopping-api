0d8cf7087054e0f11e4949765325215583f3d8c3
ampaire <femayapr@gmail.com>
2021-08-02 14:57:30 +0300

    Add changes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::UsersController < ApplicationController
     def create
         #! sign-up action
         @user = User.create(user_params)
-        @user.admin = true if ENV['USER_NAME'].include?(@user.email)
+        @user.admin = true if ENV['USERNAME'].include?(@user.email)
         if @user.valid?
             token = encode_token({ user_id: @user.id })
             render json: { id: @user.id, username: @user.username, carts: @user.carts, jwt: token }, status: :created
