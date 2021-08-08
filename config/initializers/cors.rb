3b2a426c788d0040e8067318ad7dc4d133f50383
ampaire <femayapr@gmail.com>
2021-08-08 12:30:29 +0300

    cors for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000/'
+    origins 'forna.herokuapp.com/api/v1'
 
     resource '*',
       headers: :any,
